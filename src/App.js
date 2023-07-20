e0a0de566315dc25f5a0b728a1fa5b1b909bb5cd
Candelaria Agostina Arra <candearra45@gmail.com>
2023-07-20 18:41:59 -0300

    Primer cambio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './app.css';
-import NavBar from './components/NavBar/NavBar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import "./app.css";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import NavBar from "./components/NavBar/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <NavBar></NavBar>
-      <ItemListContainer greeting="Bienvenido a mi tienda!"></ItemListContainer>
+    <div className="app-body">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:categoryId" element={<ItemListContainer />} />
+
+          <Route path="/prueba" element={<h1>Prueba</h1>} />
+          {/* Ruta con segmento URL dinamico */}
+          <Route path="/product/:id" element={<ItemDetailContainer />} />
+          <Route path="*" element={<h1>Page not found: 404</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
